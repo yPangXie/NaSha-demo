11ff9cbdfd6f4c8f25fc1fd4ccf5c190dfa4fd99
Mr.Krabs <pangxie.wdy@gmail.com>
2016-05-29 15:48:46 +0800

    feat: demo. First blood

---
+++ app.js
@@ -0,0 +1,13 @@
+"use strict"
+
+/* 基于NaSha远程命令接口, 发送微薄 */
+require('urllib').request('http://bigyoo.me/ns/cmd', {
+    "method": "POST",
+    "data": {
+        "type": "weibo",
+        "action": "sendMessage",
+        "message": "这是一条来自远程调用的微薄内容... 呵~"
+    }
+}).then((result)=> {
+    console.log(result);
+});
