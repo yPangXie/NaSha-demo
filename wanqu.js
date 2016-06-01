c90b5e5ac86483fe33cacb2d7783d42187c47345
Mr.Krabs <pangxie.wdy@gmail.com>
2016-06-01 17:40:08 +0800

    feat: 湾区日报数据获取demo

---
+++ wanqu.js
@@ -0,0 +1,28 @@
+"use strict"
+
+/* 基于NaSha远程命令接口, 获取wanqu日报数据 */
+require('urllib').request('http://bigyoo.me/ns/cmd', {
+    "method": "POST",
+    "data": {
+        "type": "wanqu",
+        "action": "getLatest"
+    }
+}).then((result)=> {
+    console.log(result);
+}).catch((err) => {
+    console.log(err);
+});
+
+/* 基于NaSha远程命令接口, 获取wanqu日报数据 */
+require('urllib').request('http://bigyoo.me/ns/cmd', {
+    "method": "POST",
+    "data": {
+        "type": "wanqu",
+        "action": "getSpec",
+        "issue": "100"
+    }
+}).then((result)=> {
+    console.log(result);
+}).catch((err) => {
+    console.log(err);
+});
