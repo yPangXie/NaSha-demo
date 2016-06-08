3a8bf00176cea1b2dc87dbd2bdbf7b83f47d5c58
Mr.Krabs <pangxie.wdy@gmail.com>
2016-06-08 16:43:03 +0800

    feat: 增加spider操作demo

--- wanqu.js
+++ wanqu.js
@@ -30,3 +30,19 @@ require('urllib').request('http://bigyoo.me:8000/ns/cmd', {
 }).catch((err) => {
     console.log(err);
 });
+
+/* 基于NaSha远程命令接口, 爬取指定地址的数据 */
+require('urllib').request('http://bigyoo.me:8000/ns/cmd', {
+    "method": "POST",
+    "timeout": 100000,
+    "data": {
+        "type": "wanqu",
+        "action": "spider",
+        "issue": "603"
+    }
+}).then((result)=> {
+    let resultData = JSON.parse(new Buffer(result.data).toString());
+    console.log(resultData);
+}).catch((err) => {
+    console.log(err);
+});
