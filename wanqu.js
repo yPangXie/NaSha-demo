cb6e8fde54779a22bdb345898ff2d013ee7f04e9
Mr.Krabs <pangxie.wdy@gmail.com>
2016-06-12 01:21:53 +0800

    feat: demo识别参数, 按需执行. 端午回家陪妹子~ hoyihoyi

--- wanqu.js
+++ wanqu.js
@@ -1,48 +1,61 @@
 "use strict"
 
+const program = require('commander');
+
+program.option('-l, --latest', 'Get latest issue of Wanqu')
+       .option('-i, --issue [issue]', 'Get the specified issue of Wanqu')
+       .option('-s, --spider [issues]', 'Grab specidied issue from Wanqu with the spider')
+       .parse(process.argv);
+
 /* 基于NaSha远程命令接口, 获取最新一期的wanqu日报数据 */
-require('urllib').request('http://bigyoo.me:8000/ns/cmd', {
-    "method": "POST",
-    "timeout": 100000,
-    "data": {
-        "type": "wanqu",
-        "action": "getLatest"
-    }
-}).then((result)=> {
-    let resultData = JSON.parse(new Buffer(result.data).toString());
-    console.log('latest:', resultData);
-}).catch((err) => {
-    console.log(err);
-});
+if(program.latest) {
+    require('urllib').request('http://bigyoo.me:8000/ns/cmd', {
+        "method": "POST",
+        "timeout": 100000,
+        "data": {
+            "type": "wanqu",
+            "action": "getLatest"
+        }
+    }).then((result)=> {
+        let resultData = JSON.parse(new Buffer(result.data).toString());
+        console.log('latest:', resultData);
+    }).catch((err) => {
+        console.log(err);
+    });
+}
 
 /* 基于NaSha远程命令接口, 获取指定期的wanqu日报数据 */
-require('urllib').request('http://bigyoo.me:8000/ns/cmd', {
-    "method": "POST",
-    "timeout": 100000,
-    "data": {
-        "type": "wanqu",
-        "action": "getSpec",
-        "issue": "1"
-    }
-}).then((result)=> {
-    let resultData = JSON.parse(new Buffer(result.data).toString());
-    console.log('spec:', resultData);
-}).catch((err) => {
-    console.log(err);
-});
+if(program.issue) {
+    require('urllib').request('http://bigyoo.me:8000/ns/cmd', {
+        "method": "POST",
+        "timeout": 100000,
+        "data": {
+            "type": "wanqu",
+            "action": "getSpec",
+            "issue": "1"
+        }
+    }).then((result)=> {
+        let resultData = JSON.parse(new Buffer(result.data).toString());
+        console.log('spec:', resultData);
+    }).catch((err) => {
+        console.log(err);
+    });
+}
 
 /* 基于NaSha远程命令接口, 爬取指定地址的数据 */
-require('urllib').request('http://bigyoo.me:8000/ns/cmd', {
-    "method": "POST",
-    "timeout": 100000,
-    "data": {
-        "type": "wanqu",
-        "action": "spider",
-        "issue": "603"
-    }
-}).then((result)=> {
-    let resultData = JSON.parse(new Buffer(result.data).toString());
-    console.log('spider:', resultData);
-}).catch((err) => {
-    console.log(err);
-});
+if(program.spider) {
+    require('urllib').request('http://bigyoo.me:8000/ns/cmd', {
+        "method": "POST",
+        "timeout": 100000,
+        "data": {
+            "type": "wanqu",
+            "action": "spider",
+            "issue": program.spider
+        }
+    }).then((result)=> {
+        let resultData = JSON.parse(new Buffer(result.data).toString());
+        console.log('spider:', resultData);
+    }).catch((err) => {
+        console.log(err);
+    });
+}
