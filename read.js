10df2543ab7d1d520c1f7f814c99d07b867d4a70
Mr.Krabs <pangxie.wdy@gmail.com>
2016-08-14 13:33:36 +0800

    feat: 增加read接口, 获取list

--- read.js
+++ read.js
@@ -3,6 +3,7 @@
 const program = require('commander');
 
 program.option('-n, --newPost', 'Store new post.')
+       .option('-l, --list', 'Get posts.')
        .parse(process.argv);
 
 /* 基于NaSha远程命令接口, 新增一篇文章 */
@@ -23,6 +24,24 @@ if(program.newPost) {
     }).then((result)=> {
         let resultData = JSON.parse(new Buffer(result.data).toString());
         console.log('newPost:', resultData);
+    }).catch((err) => {
+        console.log(err);
+    });
+}
+
+/* 基于NaSha远程命令接口, 获取指定页码的对应的数据 */
+if(program.list) {
+    require('urllib').request('http://bigyoo.me:8000/ns/cmd', {
+        "method": "POST",
+        "timeout": 100000,
+        "data": {
+            "type": "read",
+            "action": "list",
+            "limit": 5
+        }
+    }).then((result)=> {
+        let resultData = JSON.parse(new Buffer(result.data).toString());
+        console.log('list:', resultData);
     }).catch((err) => {
         console.log(err);
     });
